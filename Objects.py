8f3a346b70282eee17a2047d7046cbe6feb3b0a3
Makhmudkhuja <3110MMM3110@email.com>
2026-09-15 20:27:41 +0900

    feat:specific error handling system

--- Objects.py
+++ Objects.py
@@ -28,3 +28,25 @@ print(type(math))
 # OOP 4 concepts-> Abstraction | Encapsulation | Inheritance | Polimorphism
 result1=math.ceil(97.7) # call
 print(f"Result1:  {result1}")
+
+
+print("========= Error handling system ============")
+
+car_dict=dict(name="Tayota", year=(2026), electric=True)
+
+
+
+
+try:
+    print("Passed here")
+    a=car_dict.speed
+    result=car_dict["origin"]
+    print("Result: ", result)
+except KeyError as err:
+    print("No origin state property found: ",err)
+except AttributeError as err:
+    print("No speed found: ",err)
+else:
+    print("Executed succesfully without errors")
+finally:
+    print("Final closing logic")
